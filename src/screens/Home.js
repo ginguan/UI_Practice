400e862c3b52ef6127a370f2431f6288cc0fda2a
ginguan <yguan074@uottawa.ca>
2021-02-02 16:22:49 -0500

    update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import AwesomeSlider from 'react-awesome-slider';
 // import AutoplaySlider from 'react-awesome-slider/src/hoc/autoplay';
 import 'react-awesome-slider/dist/styles.css';
@@ -8,11 +8,13 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
 
 import './Home.css';
+import MenuDropdown from '../components/MenuDropdown'
+
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const Home= () =>{
     const TryitNow =()=>{
-        return(<button className = "btn-backup">TRY IT NOW</button>);
+        return(<button className = "btn-tryitnow">TRY IT NOW</button>);
     }
     const NavMenu =() =>{
         //f0c9
@@ -27,21 +29,27 @@ const Home= () =>{
             <div>
             <div>
             <AutoplaySlider animation="cubeAnimation"
-            play ={true}
+            // play ={true}
             cancelOnInteraction={false}
-            interval={10000}
+            // interval={10000}
             >
                 <div className="container" style ={{backgroundImage :`url('https://wallpapercave.com/wp/wp1871636.jpg')`}}>
-                <NavMenu />
-                   
+                <MenuDropdown />
+                <p className="slide-text">INTERACTIVE CONCERT EXPERIENCE</p>
+                <p className="slide-text2">Experience your favourite artists like never before and from the comfort of your own home.</p>
                 <TryitNow/>
                 </div>
                 <div className="container" style ={{backgroundImage:`url('https://th.bing.com/th/id/R1211e7557b7c3364de17047da9f33d20?rik=EeoVBGO1PDuajg&riu=http%3a%2f%2fwww.ambientlightblog.com%2fwp-content%2fuploads%2f2018%2f01%2fCigarettesAfterSex_013.jpg&ehk=5Yy0Ty%2fF8qDnnzTScwo5JJz91zKTJC0q2IJ99RiTK0E%3d&risl=&pid=ImgRaw')`}}>
-                <NavMenu/>
-                   <TryitNow/>
+                <MenuDropdown/>
+                <p className="slide-text">INTERACTIVE CONCERT EXPERIENCE</p>
+                <p className="slide-text2">Experience your favourite artists like never before and from the comfort of your own home.</p>
+                <TryitNow/>
                 </div>
                 <div className="container" style ={{backgroundImage:`url("https://i.pinimg.com/originals/20/7b/5d/207b5d20059943eb73dd5fbcd814733a.jpg")`}}>
-                <NavMenu/>
+                <MenuDropdown/>
+                <p className="slide-text">INTERACTIVE CONCERT EXPERIENCE</p>
+                <p className="slide-text2">Experience your favourite artists like never before and from the comfort of your own home.</p>
+                
                 <TryitNow/>
                 </div>
            </AutoplaySlider>
